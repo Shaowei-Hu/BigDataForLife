fed244744053ab7148d9d59c3b5776ed8760ea80
Shaowei <hushaowei0123@gmail.com>
2017-09-24 09:39:35 +0200

    Generated with JHipster 4.7.0

---
+++ src/main/java/com/shaowei/bigdatalife/security/social/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring social configuration.
+ */
+package com.shaowei.bigdatalife.security.social;
---
+++ src/main/java/com/shaowei/bigdatalife/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.shaowei.bigdatalife.web.rest.vm;
