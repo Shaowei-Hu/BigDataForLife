034a5e0790810818af02953a71598c1c599f11e1
Shaowei <hushaowei0123@gmail.com>
2017-10-17 22:06:29 +0200

    About us

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,6 +14,7 @@ import { BigDataForLifeEntityModule } from './entities/entity.module';
 import { customHttpProvider } from './blocks/interceptor/http.provider';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
 
+import { BigDataForLifeAppAboutUsModule } from './about-us/about-us.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
 import {
@@ -38,6 +39,7 @@ import {
         BigDataForLifeAdminModule,
         BigDataForLifeAccountModule,
         BigDataForLifeEntityModule,
+        BigDataForLifeAppAboutUsModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
